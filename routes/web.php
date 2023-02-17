3bcabe9ca6d44a07d0846716e242eeab3d8acdea
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-17 16:25:17 -0300

    Utilizando o belongsTo

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Produto;
+use App\Categoria;
+
+Route::get('/categorias', function () {
+    $cats = Categoria::all();
+    if (count($cats) === 0) {
+        echo "Você não possui categorias cadastradas";
+    } else {
+        foreach ($cats as $cat) {
+            echo "<p>" . $cat->id . " - " . $cat->nome . "</p>";
+        }
+    }
+});
+
+Route::get('/produtos', function () {
+    $prods = Produto::all();
+    if (count($prods) === 0) {
+        echo "<h4>Você não possui produtos cadastrados</h4>";
+    } else {
+        echo "<table>";
+        echo "<theade><tr><td>Nome</td><td>Estoque</td><td>Preco</td><td>Categoria</td></tr></theade>";
+        foreach ($prods as $p) {
+            echo "<tr>";
+                echo "<td>".$p->nome."</td>";
+                echo "<td>".$p->estoque."</td>";
+                echo "<td>".$p->preco."</td>";
+                echo "<td>".$p->categoria->nome."</td>";
+            echo "</tr>";
+        }
+    }
 });
